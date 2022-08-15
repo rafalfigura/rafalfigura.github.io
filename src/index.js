b26250822e12624acced8a4bd49540e27af201e0
Rafal Figura <rafal.figura@outlook.com>
2022-08-16 00:49:36 +0200

    Renamed public to docs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 async function getPage() {
     const { title, icon, cover, html } = await NotionPageToHtml.convert("https://www.notion.so/rfigura/61ae71a8c47d49809ffaeb9499c1ffca");
     console.log(title, icon, cover, html);
-    fs.writeFileSync('./public/index.html', html);
+    fs.writeFileSync('./docs/index.html', html);
 }
 
 getPage();
